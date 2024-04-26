b7dc4ab618f323a74e6e0fbc118fceb977b6dc63
spark-03 <>
2024-04-26 13:35:04 +0900

    第三章完成

--- Quiz/Quiz/ViewController.swift
+++ Quiz/Quiz/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.label.text = "Hello"
+        //テキストの変更
+        
     }
-
-
+    
+    @IBAction func pressButton(_ sender: Any) {
+        self.label.text = "ボタンを押しました"
+        //ボタンクリックでテキスト変更
+        self.label.textColor = UIColor.red
+        //テキストの色が赤になるように変更
+    }
+    
 }
 
